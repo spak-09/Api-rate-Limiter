d6ca820297fbbcb4531a1bdce35a9ff1640f3aa7
spak-09 <anoojkarrar@gmail.com>
2026-06-14 22:51:54 +0530

    latest

--- Frontend/src/socket/socket.js
+++ Frontend/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://api-rate-limiter-1xhr.onrender.com", {
   transports: ["websocket"],
 });
 
